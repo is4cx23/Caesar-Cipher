32ccf5e9f7d3ca08430e3081e48ba1b61ca36682
is4cx23 <00001109701159sp@al.educacao.sp.gov.br>
2024-04-16 12:20:25 -0300

    add template

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Caesar-Cipher
+
+## etapas do projeto
+
+1. criar a rota do aplicativo
+2. criar o template HTML
---
+++ caesar_cipher.py
@@ -0,0 +1,7 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def caesar_cipher(name=None):
+    return render_template('index.html', name=name)
