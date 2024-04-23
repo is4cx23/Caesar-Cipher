827221e91cc28b0f9e6cd8d7048e0f3e7c4719bc
is4cx23 <00001109701159sp@al.educacao.sp.gov.br>
2024-04-23 12:25:43 -0300

    adiciona rota encode

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -3,5 +3,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def caesar_cipher(name=None):
+def home (name=None):
     return render_template('index.html', name=name)
+@app.route("/encode")
+def encode (name=None):
+    return render_template('encode.html', name=name)
