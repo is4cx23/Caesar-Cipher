eebbec46d97ef7ba31ab430f8b75cdda4e019a74
is4cx23 <00001109701159sp@al.educacao.sp.gov.br>
2024-05-02 12:10:44 -0300

    finishes encode

--- app/encode/__init__.py
+++ app/encode/__init__.py
@@ -1,3 +1,31 @@
-def encode_function(message, shift):
+def encode(message, shift):
+    alphabet =['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     
-    return message
+    partialStart = ''
+    partialend = ''
+    shiftedAlphabet= ''
+    encodedMessage = ''
+
+    if shift == 0:
+        shiftedAlphabet = alphabet
+    elif shift > 0 and shift < 26:
+        partialStart = alphabet[:shift]
+        partialend = alphabet[:shift]
+        shiftedAlphabet = partialend + partialStart
+    elif shift < 0 and shift > -26:
+        shift = shift * (-1)
+        partialStart = alphabet[:shift]
+        partialend = alphabet[:shift]
+        shiftedAlphabet = partialend + partialStart
+    else:
+     return("shift_error")
+ 
+    for letter in alphabet:
+       letter_index = alphabet.index(letter)
+       encodedMessage = encodedMessage + shiftedAlphabet[letter_index]
+
+
+
+    return encodedMessage 
+    
+encode("BDE", 3)
